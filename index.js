747ff8e2af3905b39c0c6bf78803eef9fec6978f
Andrey <lopatkin.andrushka@yandex.ru>
2021-09-08 14:04:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const showPrevBtn = document.getElementById('show-prev-btn');
+const showNextBtn = document.getElementById('show-next-btn');
+
+showPrevBtn.addEventListener('click', onShowPrevBtnClick);
+showNextBtn.addEventListener('click', onShowNextBtnClick);
+
+let imgsUrls
+
+
+
+
+
+
+
+
+
+
+
+
+
+function onShowPrevBtnClick(){
+    console.log('prev cliked');
+}
+
+function onShowNextBtnClick(){
+    console.log('next cliked');
+}
