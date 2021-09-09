09878e8126fa458870d0f946e6b68ccb04be1c55
lopatinAA <lopatkin.andrushka@yandex.ru>
2021-09-09 14:30:08 +0300

    finished slider

--- index.js
+++ index.js
@@ -1,27 +1,56 @@
+//find elements
 const showPrevBtn = document.getElementById('show-prev-btn');
 const showNextBtn = document.getElementById('show-next-btn');
+const slideImage = document.getElementById('slide-img');
 
+//subscribe to events
 showPrevBtn.addEventListener('click', onShowPrevBtnClick);
 showNextBtn.addEventListener('click', onShowNextBtnClick);
 
-console.log('test')
-
-console.log('test2');
-
-
-
-
+// crete images array
+let imagesUrls = [];
+imagesUrls.push('imgs/img_1231.png');
+imagesUrls.push('imgs/img_1232.jpg');
+imagesUrls.push('imgs/img_1233.jpg');
 
 
+let currentImageIndex = 0 ;
+showPrevBtn.disabled = true;
+
+slideImage.src = imagesUrls[currentImageIndex];
 
 
 
 
 
+
+
+
+
+
+
+
+//function definitions
 function onShowPrevBtnClick(){
     console.log('prev cliked');
+    currentImageIndex--;
+    slideImage.src = imagesUrls[currentImageIndex];
+    showNextBtn.disabled = false;
+    
+    //disabled prev btn if need
+    if (currentImageIndex === 0) {
+        showPrevBtn.disabled = true;
+    }
 }
 
 function onShowNextBtnClick(){
     console.log('next cliked');
+    currentImageIndex++;
+    slideImage.src = imagesUrls[currentImageIndex];
+    showPrevBtn.disabled = false;
+
+    //disbled next btn if need
+    if (currentImageIndex === (imagesUrls.length - 1)) {
+        showNextBtn.disabled = true;
+    }
 }
